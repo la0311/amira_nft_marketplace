9a0c2e37ee080721943c0f447eb054028c50bfda
La <35848139+la0311@users.noreply.github.com>
2023-02-19 17:02:47 +0700

    Create basic page structure and route

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 const Home = () => (
   <div>
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <h1 className="text-3xl font-bold
+    underline"
+    >Hello world!
+    </h1>
   </div>
 );
 
