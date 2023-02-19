cde5a16b95b1d795c96ab83482c65788fa566b13
La <35848139+la0311@users.noreply.github.com>
2023-02-20 00:16:05 +0700

    Create re-usable component and package to _app.js
    Including:
    Navbar, Footer component.
    ThemeProvider for light/dark mode.
    Fontawesome for icon.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { Navbar, Footer } from '../components';
+
+import '../styles/globals.css';
+
+const App = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+
+    <Script src="https://kit.fontawesome.com/359807a1ab.js" crossOrigin="anonymous" />
+  </ThemeProvider>
+);
+
+export default App;
