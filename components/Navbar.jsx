dadd9bc0ac2830af13c2b6d65fc6f78e1c2164c5
La <35848139+la0311@users.noreply.github.com>
2023-02-20 23:53:57 +0700

    Import react hook and component, assets

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
 
-const Navbar = () => (
-  <div>Navbar</div>
-);
+import Image from 'next/image';
+import Link from 'next/link';
+
+import images from '../assets';
+
+const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+
+
+  return (
+    <div>Navbar</div>);
+};
 
 export default Navbar;
