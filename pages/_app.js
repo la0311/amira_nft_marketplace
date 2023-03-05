c1cdf9753c94243f419e0c2690a00782722c80a2
La <35848139+la0311@users.noreply.github.com>
2023-03-05 21:05:43 +0700

    Implement NFT Context to share nftCurrency

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,25 @@
 import { ThemeProvider } from 'next-themes';
 import Script from 'next/script';
+import { NFTProvider } from '../context/NFTContext';
 
 import { Navbar, Footer } from '../components';
 
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }) => (
-  <ThemeProvider attribute="class">
-    <div className="dark:bg-nft-dark bg-white min-h-screen">
-      <Navbar />
-      <div className="pt-5">
-        <Component {...pageProps} />
+  <NFTProvider>
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Navbar />
+        <div className="pt-5">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
 
-    <Script src="https://kit.fontawesome.com/359807a1ab.js" crossOrigin="anonymous" />
-  </ThemeProvider>
+      <Script src="https://kit.fontawesome.com/359807a1ab.js" crossOrigin="anonymous" />
+    </ThemeProvider>
+  </NFTProvider>
 );
 
 export default App;
