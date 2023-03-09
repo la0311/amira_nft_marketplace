9c9e7d5a463ce5c229786399d046dc5e771d1a43
La <35848139+la0311@users.noreply.github.com>
2023-03-10 01:36:17 +0700

    Update Top Creators with real data

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import images from '../assets';
 import { CreatorCard, Banner, NFTCard } from '../components';
 import { makeId } from '../utils/makeId';
 import { NFTContext } from '../context/NFTContext';
+import { getCreators } from '../utils/getTopCreators';
+import { shortenAddress } from '../utils/shortenAddress';
 
 const Home = () => {
   const [hideButtons, setHideButtons] = useState(false);
@@ -51,6 +53,8 @@ const Home = () => {
     };
   });
 
+  const topCreators = getCreators(nfts);
+
   return (
     <div className="flex justify-center sm:px-4 p-12">
       <div className="w-full minmd:w-4/5">
@@ -65,7 +69,16 @@ const Home = () => {
 
           <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
             <div className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none" ref={scrollRef}>
-              {[6, 7, 8, 9, 10].map((i) => (
+              {topCreators.map((creator, i) => (
+                <CreatorCard
+                  key={creator.seller}
+                  rank={i + 1}
+                  creatorImage={images[`creator${i + 1}`]}
+                  creatorName={shortenAddress(creator.seller)}
+                  creatorEths={creator.sum}
+                />
+              ))}
+              {/* {[6, 7, 8, 9, 10].map((i) => (
                 <CreatorCard
                   key={`creator${i}`}
                   rank={i}
@@ -73,7 +86,7 @@ const Home = () => {
                   creatorName={`0x${makeId(3)}...${makeId(4)}`}
                   creatorEths={10 - i * 0.5}
                 />
-              ))}
+              ))} */}
 
               {!hideButtons && (
               <>
